53980875c34e53a60dc89a4e954a7153f1d70b8a
Yatharthg12 <72456920+Yatharthg12@users.noreply.github.com>
2021-10-22 17:51:33 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,7 @@
+def atm():
+    cardnumber = input("Enter your atm card number")
+    pinnumber = input("Enter your pin number")
+    withdraw = input("Would you like to withdraw cash? If yes, reply with the amount")
+    thanks = input("Thanks!")
+
+atm()
